1fdf8a4c95725494eee7f838c5eff280d5e2d5a5
liuguoqing@sxwdsoft.com <liuguoqing@sxwdsoft.com>
2019-03-06 15:48:28 +0800

    首次提交微服务代码

---
+++ src/main/java/com/example/demo/FcsServerTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcsServerTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcsServerTwoApplication.class, args);
+	}
+
+}
